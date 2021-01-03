1a6c23bf9c00e32c3a48ce6fd904dd49c037243f
dlee67 <keyboardPuncher96@gmail.com>
2021-01-03 03:41:32 -0700

    When is his Blender thingy going to be done ...

--- app/src/main/java/com/example/bunkerbeacon/HomeFragment.kt
+++ app/src/main/java/com/example/bunkerbeacon/HomeFragment.kt
@@ -4,9 +4,9 @@ import android.app.AlarmManager
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
-import android.content.Context.ALARM_SERVICE
-import android.content.Context.NOTIFICATION_SERVICE
+import android.content.Context.*
 import android.content.Intent
+import android.content.SharedPreferences
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
@@ -17,9 +17,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import java.util.Calendar
 import java.util.concurrent.TimeUnit
+import java.util.TimeZone;
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -42,6 +45,10 @@ class HomeFragment : Fragment() {
     lateinit var notificationManager: NotificationManager
     lateinit var firstNumberView: EditText
     lateinit var secondNumberView: EditText
+    lateinit var daysBeforePanicView: EditText
+    lateinit var panicDate: TextView
+
+    var preferences: SharedPreferences? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,33 +67,58 @@ class HomeFragment : Fragment() {
         var alarmManager = context?.getSystemService(ALARM_SERVICE) as AlarmManager
         firstNumberView = homeFragment.findViewById(R.id.first_phone_number)
         secondNumberView = homeFragment.findViewById(R.id.second_phone_number)
+        daysBeforePanicView = homeFragment.findViewById(R.id.amt_of_days_before_panic)
+        panicDate = homeFragment.findViewById(R.id.amt_until_panic_text)
+        preferences = activity?.getPreferences(MODE_PRIVATE)
+
+        panicDate.text = preferences?.getString("persist-string-panic-date",
+                "Calm like the falling snow")
 
         homeFragment.findViewById<Button>(R.id.sendButton).setOnClickListener {
             Log.i("dhl", "Sending message")
 
             var firstNumber = firstNumberView.text
             var secondNumber = secondNumberView.text
-            Log.i("dhl", "first number at: " + firstNumber)
             var notifyIntent = Intent(context, AlarmReceiver::class.java)
             notifyIntent.putExtra("firstNumber", firstNumber.toString())
             notifyIntent.putExtra("secondNumber", secondNumber.toString())
             var pendingIntent = PendingIntent.getBroadcast(context, NOTIFICATION_ID, notifyIntent,
                     PendingIntent.FLAG_UPDATE_CURRENT)
-            var amountOfDays = Integer(homeFragment.findViewById<EditText>(R.id.second_phone_number)
-                    .text.toString());
 
-            var waitInterval = TimeUnit.DAYS.toMillis(amountOfDays.toLong());
+            if(daysBeforePanicView.text.toString() != "" &&
+                (firstNumber.toString() != "" || secondNumber.toString() != "")) {
+                var amountOfDays = daysBeforePanicView.text.toString()
+                var preferenceEdit = preferences?.edit()
+                Log.i("dhl", "Amount of days before panic at: " + amountOfDays)
 
-            if (alarmManager != null) {
-                alarmManager?.setExactAndAllowWhileIdle(
-                        AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                        // elapsedRealtime() is required to actually the correct amount of pass.
-                        SystemClock.elapsedRealtime() + waitInterval,
-                        pendingIntent)
+                var calendar = Calendar.getInstance()
+                calendar.add(Calendar.DATE, daysBeforePanicView.text.toString().toInt())
+                panicDate.text = "Will panic on: " + calendar.time.toString()
+                preferenceEdit?.putString("persist-string-panic-date", "Will panic on: " + calendar.time.toString())
+                preferenceEdit?.commit()
+
+                Log.i("dhl", "Calendar at: " + calendar.time.toString());
+
+                var waitInterval = TimeUnit.DAYS.toMillis(amountOfDays.toLong())
+
+                if (alarmManager != null) {
+                    alarmManager?.setExactAndAllowWhileIdle(
+                            AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                            // elapsedRealtime() is required to actually the correct amount of pass.
+                            SystemClock.elapsedRealtime() + waitInterval,
+                            pendingIntent)
+                }
+
+                Toast.makeText(context,
+                        "Don't forget to check-in daily",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context,
+                        "Gotta have the amount of days and gotta at least have one phone number",
+                        Toast.LENGTH_SHORT).show()
             }
-
-            Toast.makeText(context, "Test set", Toast.LENGTH_SHORT).show();
         }
+
         createNotificationChannel()
 
         // Inflate the layout for this fragment
